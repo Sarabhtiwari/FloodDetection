805ca146c79788ffc1d55511100fbaf26372fe1c
Brimhesh Tiwari <tiwaribrimhesh@gmail.com>
2025-11-04 15:10:53 +0530

    some corrections

--- backend/app.py
+++ backend/app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from datetime import datetime
 import numpy as np
-from ext_data import send_remaining_data
+from .ext_data import send_remaining_data
 # --- Configuration & Model Loading ---
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), "static"), static_url_path="")
 CORS(app, origins=['*'])  
